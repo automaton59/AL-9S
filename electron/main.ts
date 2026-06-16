0758b07759856a546ff31d66ba140bf0d20878d7
AL-1S <al1s@example.com>
2026-06-16 19:00:24 +0800

    fix(electron): correct production HTML path from dist-react to dist

--- electron/main.ts
+++ electron/main.ts
@@ -18,7 +18,7 @@ function createWindow() {
     win.loadURL('http://localhost:5173');
     win.webContents.openDevTools();
   } else {
-    win.loadFile(path.join(__dirname, '../dist-react/index.html'));
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
   }
 }
 
